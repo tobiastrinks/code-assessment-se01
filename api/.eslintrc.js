ae29a333c409e07ed79700e6987a17f2ce7cae16
tobiastrinks <tobias.trinks@code.berlin>
2018-04-05 16:18:33 +0200

    - added node_modules

---
+++ api/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "indent": ["error", 4],
+        "semi": ["error", "always"],
+        "no-trailing-spaces": ["error", { "skipBlankLines": true }],
+        allowEmptyReject: true
+    }
+};
